6635d6c5df57f02adf08419ff9d5fc59fbbb6232
[Assassin] <ShyAssassin@assassin.dev>
2025-07-17 00:28:55 +0200

    Flake init

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs, ... }: let
+    systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+    pkgsFor = system: import nixpkgs {
+      inherit system;
+    };
+  in {
+    devShells = forAllSystems (system: let
+      pkgs = pkgsFor system;
+      dotnet_sdk = pkgs.dotnet-sdk_8;
+    in {
+      default = pkgs.mkShell rec {
+        buildInputs = with pkgs; [
+          cmake opencv
+          dotnet_sdk fontconfig
+          libjpeg onnxruntime libGL
+          xorg.libX11 xorg.libSM xorg.libICE
+        ];
+
+        shellHook = ''
+          export DOTNET_ROOT="${dotnet_sdk}"
+          # Fucks knows why C# looks in path instead of LD_LIBRARY_PATH but it does
+          export PATH="$PATH:${builtins.toString (pkgs.lib.makeLibraryPath buildInputs)}";
+          export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${builtins.toString (pkgs.lib.makeLibraryPath buildInputs)}";
+        '';
+      };
+    });
+  };
+}
