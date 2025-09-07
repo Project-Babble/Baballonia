f0062971f527d83fcf0098be3e11f5b812cee097
RedInJector <imaxprots@gmail.com>
2025-09-08 01:48:48 +0200

    feat: check for updates rough sketch

---
+++ src/Baballonia/Controls/DropdownMessage.axaml.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.ObjectModel;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Baballonia.Controls;
+
+
+public class ButtonAction
+{
+    public string Text { get; set; } = string.Empty;
+    public IRelayCommand? Command { get; set; }
+}
+
+public partial class DropdownMessage : UserControl
+{
+    public static readonly StyledProperty<IImage?> ImageSourceProperty =
+        AvaloniaProperty.Register<DropdownMessage, IImage?>(nameof(ImageSource));
+    public static readonly StyledProperty<string> TitleProperty =
+        AvaloniaProperty.Register<DropdownMessage, string>(nameof(Title), string.Empty);
+    public static readonly StyledProperty<string> MessageProperty =
+        AvaloniaProperty.Register<DropdownMessage, string>(nameof(Message), string.Empty);
+    public static readonly StyledProperty<string> AcceptProperty =
+        AvaloniaProperty.Register<DropdownMessage, string>(nameof(AcceptText), string.Empty);
+    public static readonly StyledProperty<string> DeclineProperty =
+        AvaloniaProperty.Register<DropdownMessage, string>(nameof(DeclineText), string.Empty);
+    public static readonly StyledProperty<IRelayCommand?> AcceptCommandProperty =
+        AvaloniaProperty.Register<DropdownMessage, IRelayCommand?>(nameof(AcceptCommand));
+    public static readonly StyledProperty<IRelayCommand?> DeclineCommandProperty =
+        AvaloniaProperty.Register<DropdownMessage, IRelayCommand?>(nameof(DeclineCommand));
+    public IImage? ImageSource
+    {
+        get => GetValue(ImageSourceProperty);
+        set => SetValue(ImageSourceProperty, value);
+    }
+    public string Title
+    {
+        get => GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
+    public string Message
+    {
+        get => GetValue(MessageProperty);
+        set => SetValue(MessageProperty, value);
+    }
+    public string AcceptText
+    {
+        get => GetValue(AcceptProperty);
+        set => SetValue(AcceptProperty, value);
+    }
+    public string DeclineText
+    {
+        get => GetValue(DeclineProperty);
+        set => SetValue(DeclineProperty, value);
+    }
+
+    public IRelayCommand? AcceptCommand
+    {
+        get => GetValue(AcceptCommandProperty);
+        set => SetValue(AcceptCommandProperty, value);
+    }
+    public IRelayCommand? DeclineCommand
+    {
+        get => GetValue(DeclineCommandProperty);
+        set => SetValue(DeclineCommandProperty, value);
+    }
+
+    public DropdownMessage()
+    {
+        InitializeComponent();
+    }
+}
+
---
+++ src/Baballonia/Services/UpdateService.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Baballonia.Services;
+
+public class UpdateService
+{
+    private string _url = "https://api.github.com/repos/Project-Babble/Baballonia/releases/latest";
+
+    public async Task<string?> GetLatestVersion()
+    {
+        using var httpClient = new HttpClient();
+        httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("Baballonia");
+
+        var rawJson = await httpClient.GetStringAsync(_url);
+
+        var json = JsonDocument.Parse(rawJson);
+
+        var tagName = json.RootElement.GetProperty("tag_name").GetString();
+
+        return tagName?.TrimStart('v');
+    }
+
+}
