702981ae05e47b5d4273683a21a5dd4308756672
Fro Zen <frozenreflex@frozenreflex.net>
2025-11-13 17:59:31 -0600

    Set up ARM builds

--- download_dependencies.sh
+++ download_dependencies.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-curl -Z -L "https://github.com/Project-Babble/BabbleTrainer/releases/latest/download/BabbleTrainer" -L "https://github.com/Project-Babble/BabbleCalibration/releases/latest/download/Linux.zip" -L "https://github.com/esp-rs/espflash/releases/latest/download/espflash-x86_64-unknown-linux-gnu.zip" -o "src/Baballonia.Desktop/Calibration/Linux/Trainer/BabbleTrainer" -o "src/Baballonia.Desktop/Calibration/Linux/Overlay/Linux.zip" -o "src/Baballonia/Firmware/Linux/espflash-x86_64-unknown-linux-gnu.zip"
+calibrationDownloadPath="https://github.com/Project-Babble/BabbleCalibration/releases/latest/download/Linux.zip"
+trainerDownloadPath="https://github.com/Project-Babble/BabbleTrainer/releases/latest/download/BabbleTrainer-x64"
+espflashDownloadPath="https://github.com/esp-rs/espflash/releases/latest/download/espflash-x86_64-unknown-linux-gnu.zip"
+
+isArm=$(uname -m)
+if [[ $isArm == *"arm"* ]]; then
+  calibrationDownloadPath="https://github.com/Project-Babble/BabbleCalibration/releases/latest/download/Linux-ARM.zip"
+  trainerDownloadPath="https://github.com/Project-Babble/BabbleTrainer/releases/latest/download/BabbleTrainer-arm"
+  espflashDownloadPath="https://github.com/esp-rs/espflash/releases/latest/download/espflash-aarch64-unknown-linux-gnu.zip"
+fi
+
+curl -Z -L $trainerDownloadPath -L $calibrationDownloadPath -L $espflashDownloadPath -o "src/Baballonia.Desktop/Calibration/Linux/Trainer/BabbleTrainer" -o "src/Baballonia.Desktop/Calibration/Linux/Overlay/Linux.zip" -o "src/Baballonia/Firmware/Linux/espflash.zip"
 
 unzip "src/Baballonia.Desktop/Calibration/Linux/Overlay/Linux.zip" -d "src/Baballonia.Desktop/Calibration/Linux/Overlay"
 rm "src/Baballonia.Desktop/Calibration/Linux/Overlay/Linux.zip"
 
-unzip "src/Baballonia/Firmware/Linux/espflash-x86_64-unknown-linux-gnu.zip" -d "src/Baballonia/Firmware/Linux"
-rm "src/Baballonia/Firmware/Linux/espflash-x86_64-unknown-linux-gnu.zip"
+unzip "src/Baballonia/Firmware/Linux/espflash.zip" -d "src/Baballonia/Firmware/Linux"
+rm "src/Baballonia/Firmware/Linux/espflash.zip"
